6c84eb7a97adbecdfbd328d8c7e692a498c40e0d
Md Ibrahim <mdrafi15429@gmail.com>
2022-06-30 16:30:43 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import { Routes, Route,} from "react-router-dom";
 import Navber from './pages/Shared/Navber';
+import Footer from './pages/Shared/Footer';
 function App() {
   return (
     <div>
       <Navber />
       <Routes></Routes>
+      <Footer />
     </div>
   );
 }
